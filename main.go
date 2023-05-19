13ec23f06d1a623955af623f963ba0c9d278f0fa
daveetheknave8 <davidsreeves1@gmail.com>
2023-05-19 12:50:52 -0700

    create get objects endpoint

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fmt.Println(sendProjects())
+}
+
+func getProjects(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, sendProjects())
 }
