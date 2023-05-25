7f622519710446773e442a48efc1adb5c80e2d4f
daveetheknave8 <davidsreeves1@gmail.com>
2023-05-24 21:11:07 -0700

    add get route for project by ID

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	router := gin.Default()
 	router.GET("/projects", getProjects)
+	router.GET("/projects/:id", getProjectById)
 	router.POST("/projects", postProjects)
 
 	router.Run("localhost:8080")
@@ -16,6 +17,16 @@ func main() {
 
 func getProjects(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, sendProjects())
+}
+
+func getProjectById(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range sendProjects() {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+		}
+	}
 }
 
 func postProjects(c *gin.Context) {
