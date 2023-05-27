812fbd5edc70965becbcca11684dd25c096a67fd
daveetheknave8 <davidsreeves1@gmail.com>
2023-05-27 15:14:56 -0700

    add calls to db

--- project.go
+++ project.go
@@ -2,7 +2,7 @@ package main
 
 // project represents a personal project developed or contributed by me
 type project struct {
-	ID          string   `json:"id"`
+	ID          int      `json:"id"`
 	Title       string   `json:"title"`
 	Description string   `json:"description"`
 	Links       []string `json:"links"`
